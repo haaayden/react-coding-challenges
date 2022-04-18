d7f81663027e0140572d7b49ae4fb5bf34ce2e88
Hayden <747644-haayden@users.noreply.gitlab.com>
2022-04-18 18:47:21 +0100

    My Spootify Coding Challenge solution

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -3,6 +3,8 @@ export default {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
     clientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+    // Put secret in `spootify\.env` file using Get Token from doc page e.g.:
+    // https://developer.spotify.com/console/get-new-releases/
     clientSecret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
   }
 }
